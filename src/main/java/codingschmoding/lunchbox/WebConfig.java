f98f8a5ecb12a5015e3d643ed5ac7c052c7c414e
TiinaSeppala <tiina.seppala@myy.haaga-helia.fi>
2018-12-10 09:44:53 +0200

    access_controll_allow-lisäys

--- src/main/java/codingschmoding/lunchbox/WebConfig.java
+++ src/main/java/codingschmoding/lunchbox/WebConfig.java
@@ -1,7 +1,16 @@
 package codingschmoding.lunchbox;
 
+import java.io.IOException;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,14 +23,12 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter {
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
             	//.antMatchers("/responses", "/responses/{id}", "/responselist").permitAll()
-            	// HOX POX!
-            	//nämä pitää vielä pohtia miten halutaan näkyviin!!!!!
                 .antMatchers("/questions", "/questions/{id}", "/questions-no-options", "/questions-no-options/{id}", "/questionoptions").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -33,6 +40,17 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
             	.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/home")
                 .permitAll();
     }
+        public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException, ServletException {
+            if(HttpMethod.OPTIONS.matches(request.getMethod())){
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, request.getHeader(HttpHeaders.ORIGIN));
+                response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS));
+                response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD));
+                response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+            }else{
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+            }
+        }
 
     @Bean
     @Override
